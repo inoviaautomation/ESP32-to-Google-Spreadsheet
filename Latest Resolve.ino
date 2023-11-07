da2ca4a46cd40a8639751f853fa8fb1fe57932aa
inoviaautomation <149832367+inoviaautomation@users.noreply.github.com>
2023-11-07 21:03:51 +0530

    Update Latest Resolve.ino

--- Latest Resolve.ino
+++ Latest Resolve.ino
@@ -1,22 +1,22 @@
 #include <ModbusRTU.h>
-/Definitions/
+//Definitions/
 #define BAUD_RATE 9600
 #define RS485_MODE 15
 #define SLAVE_ID 1
-/Instances/
+//Instances/
 ModbusRTU mb;
 //SoftwareSerial mySerial(RS485_RX, RS485_TX);
-/Instances END/
-/Variables START/
+//Instances END/
+//Variables START/
 uint16_t myWord;
-/Variables END/
-/Modbus Call back function for ERROR/
+//Variables END/
+//Modbus Call back function for ERROR/
 bool cb(Modbus::ResultCode event, uint16_t transactionId, void* data)
 {
 Serial.printf_P("Request result: 0x%02X, Mem: %d\n", event, ESP.getFreeHeap());
 return true;
 }
-/Void Setup START/
+//Void Setup START/
 void setup()
 {
 Serial.begin(BAUD_RATE);
@@ -27,8 +27,8 @@ Serial.println();
 mb.begin(&Serial2, RS485_MODE);
 mb.master();
 }
-/Void Setup END/
-/Void Loop START/
+//Void Setup END/
+//Void Loop START/
 void loop()
 {
 mb.readHreg(SLAVE_ID , 7, &myWord, 1, cb);//Address 40007 for displaying SLAVE ID (check the energy meter datasheet above)
@@ -38,4 +38,4 @@ mb.task();
 //yield();
 delay(500);
 }
-/Void Loop END/`
+//Void Loop END/`
